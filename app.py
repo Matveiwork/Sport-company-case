a79f38ada887e56bdc64b382ce9cb1fd4157f6df
Matvei Terebilenko <165888577+Matveiwork@users.noreply.github.com>
2024-08-18 23:39:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import streamlit as st
 import pandas as pd
 
@@ -11,17 +5,13 @@ st.title("A Simple Streamlit Web App")
 
 name = st.text_input('Enter your name', '')
 
-st.write(f”Hello {name}!')
+st.write(f'Hello {name}!')
 
-x = st.slider(“Select an integer x”, 0, 10, 1)
-y = st.slider(“Select an integer y”, 0, 10, 1)
+x = st.slider('Select an integer x', 0, 10, 1)
+y = st.slider('Select an integer y', 0, 10, 1)
 
-df = pd.DataFrame({“x”: [x], “y”: [y] , “x + y”: [x + y]}, index = [“addition row”])
+df = pd.DataFrame({'x': [x], 'y': [y] , 'x + y': [x + y]}, index = ['addition row'])
 st.write(df)
-
-
-# In[ ]:
-
 
 
 
